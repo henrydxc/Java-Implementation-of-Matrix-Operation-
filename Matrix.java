e2cee368da33ff296901c40f4a99f72ebcdb6b6e
XiangCheng Ding <prcding@gmail.com>
2024-02-04 13:36:18 +0800

    Add files via upload

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,7 @@
+/**
+ * @author Xiangcheng Ding
+ */
+
 import java.util.Arrays;
 
 public class Matrix {
@@ -32,8 +36,7 @@ public class Matrix {
         }
     }
 
-    public void REF() {
-        //int [][] result = new int[row][column];
+    public void GaussElimination() {
         int rowCnt = 0, temp = 0;
         while (rowCnt < row - 1) {
             temp = rowCnt + 1;
@@ -52,7 +55,6 @@ public class Matrix {
                 rowMultiply(r, 1 / matrix[r][leIndex]);
             }
         }
-        // uncompleted
     }
     public void zeroRowGoesDown() {
         for (int i=0; i<row-1; i++) {
@@ -71,6 +73,85 @@ public class Matrix {
             }
         }
         return column;
+    }
+
+    public void GaussJordanElimination() {
+        GaussElimination();
+        int cnt = 1;
+        while (cnt < row) {
+            int le = checkLeadingEntry(cnt);
+            for (int i=0; i<cnt; i++) {
+                rowReduction(i, matrix[i][le] / matrix[cnt][le], cnt);
+            }
+            cnt++;
+        }
+    }
+
+    public Matrix transpose(){
+        double [][] t = new double[column][row];
+        for (int r = 0; r < column; r++) {
+            for (int c = 0; c < row; c++) {
+                t[r][c] = matrix[c][r];
+            }
+        }
+        return new Matrix(t);
+    }
+
+    public Matrix inverse() {
+        if (!invertible()){
+            throw new RuntimeException("This matrix is not invertible");
+        }
+        double [][] inverse = new double[row][column];
+        double [][] equation = new double[row][2*column];
+        for (int i=0; i<row; i++) {
+            equation[i] = Arrays.copyOf(matrix[i], 2*column);
+            equation[i][column+i] = 1;
+        }
+        Matrix system = new Matrix(equation);
+        system.GaussJordanElimination();
+        for (int i=0; i<row; i++) {
+            inverse[i] = Arrays.copyOfRange(system.matrix[i],column,2*column);
+        }
+        return new Matrix(inverse);
+    }
+
+    public boolean invertible() {
+        if (column != row) {
+            return false;
+        }
+        if (getDeterminant() == 0) {
+            return false;
+        }
+        return true;
+    }
+    public double getDeterminant() {
+        if (row == 1)
+            return matrix[0][0];
+        double det = 0;
+        for (int i=0; i<column;i++){
+            double entry = matrix[0][i];
+            Matrix sub = subMatrix(i);
+            int sign = (i % 2 == 0) ? 1 : -1;
+            det += sign * entry * sub.getDeterminant();
+        }
+        return det;
+    }
+
+    public Matrix subMatrix(int c) {
+        Matrix sub = new Matrix(row-1,column-1);
+        int j=0,k=0;
+        for (int i=0; i<column; i++){
+            if (i == c){
+                continue;
+            }
+            j = 0;
+            while (j < sub.row) {
+                sub.matrix[j][k] = matrix[j+1][i];
+                j++;
+            }
+            k++;
+        }
+        return sub;
     }
 
     public void display(){
